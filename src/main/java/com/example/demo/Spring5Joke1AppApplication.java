22f3412b6e3c497757ddf5864785a951a63d6d32
Olaff Molero <olaffmartin@gmail.com>
2017-09-25 08:25:52 -0500

    add Java class configuration and xml configuration

--- src/main/java/com/example/demo/Spring5Joke1AppApplication.java
+++ src/main/java/com/example/demo/Spring5Joke1AppApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+//@ImportResource("classpath:joke-config.xml") //xml config
 public class Spring5Joke1AppApplication {
 
 	public static void main(String[] args) {
